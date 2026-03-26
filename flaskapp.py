547ec719bc9b4858c6e6155d7c3697d6b6ec52ba
Sylvia Brown <sylviabrown@Sylvias-MacBook-Air.local>
2026-03-25 21:52:32 -0500

    Add time query

--- flaskapp.py
+++ flaskapp.py
@@ -127,6 +127,29 @@ def price_form_post():
 
 # TODO: Section 3 — add your /timequerytextbox GET and POST routes here
 
+@app.route("/timequery/<time>")
+def viewtime(time):
+    rows = execute_query("""
+        SELECT ArtistId, Artist.Name, Track.Name, UnitPrice, Milliseconds
+        FROM Artist
+        JOIN Album USING (ArtistID)
+        JOIN Track USING (AlbumID)
+        WHERE Milliseconds > %s
+        ORDER BY Milliseconds DESC
+        LIMIT 500
+    """, (str(time),))
+    return display_html(rows)
+
+@app.route("/timequerytextbox", methods=['GET'])
+def time_form():
+
+    return render_template('textbox.html', fieldname="Time")
+
+@app.route("/timequerytextbox", methods=['POST'])
+def time_form_post():
+    text = request.form['text']
+    return viewtime(text)
+
 
 # ---------------------------------------------------------------------------
 # Run the app
